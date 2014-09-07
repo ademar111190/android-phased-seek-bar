c4731a652ffbf02e170a42053a4dc0329d286d6a
Ademar Alves de Oliveira <ademar111190@gmail.com>
2014-09-07 14:28:56 -0300

    add phased seek bar library project

--- sample/src/main/java/ademar/phasedseekbar/sample/SampleActivity.java
+++ sample/src/main/java/ademar/phasedseekbar/sample/SampleActivity.java
@@ -1,4 +1,4 @@
-package ademar.phasedseekbar;
+package ademar.phasedseekbar.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -8,7 +8,7 @@ public class SampleActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sample_activity);
+        setContentView(ademar.phasedseekbar.R.layout.sample_activity);
     }
 
 }
